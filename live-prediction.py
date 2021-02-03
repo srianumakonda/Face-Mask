b35abbdac8f66cab684b62c492a9b77337058cfd
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-02-03 09:40:48 -0500

    Update live-prediction.py

--- live-prediction.py
+++ live-prediction.py
@@ -8,16 +8,17 @@ import time
 
 img_size = 128
 cap = cv2.VideoCapture(0)
-load_model = tf.keras.models.load_model("transfer-learning-model.h5",custom_objects={'KerasLayer': hub.KerasLayer})
-# load_model = tf.keras.models.load_model("model.h5")
+# load_model = tf.keras.models.load_model("model.h5",custom_objects={'KerasLayer': hub.KerasLayer})
+load_model = tf.keras.models.load_model("model.h5")
 labels = {0:"No Mask", 1:"Mask"}
 
 while True:
     ret, frame = cap.read()
     resize_img = cv2.resize(frame,(img_size,img_size))
-    prediction = load_model.predict(np.array(resize_img).reshape(1,img_size,img_size,3))
+    img_for_model = resize_img / 255.0
+    prediction = load_model.predict(np.array(img_for_model).reshape(1,img_size,img_size,3))
     cv2.imshow("frame",resize_img)
-    print(f"Predicted result:{labels[np.argmax(prediction)]}")
+    print(f"Predicted result:{prediction}")
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break

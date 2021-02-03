ee606d5e756ad466b5786ad514cecfef3468520f
Sri Anumakonda <54557955+srianumakonda@users.noreply.github.com>
2021-02-03 09:40:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,101 +1,120 @@
+#Put code below if u don't want tensorflow warnings
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, Dropout, MaxPooling2D, BatchNormalization, Dense, Flatten, Activation
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import TensorBoard
-import tensorflow_addons as tfa
 from PIL import Image, ImageOps
 import time
-
 from sklearn.metrics import confusion_matrix
 
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D, Dropout, MaxPooling2D, Dense, Flatten, Activation, BatchNormalization
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
 
-train_dir = ".\\dataset\\Train\\"
-val_dir = ".\\dataset\\Validation\\"
-test_dir= ".\\dataset\\Test\\"
-
-img_size = 128
-batch_size = 64
+img_size = 224
+batch_size = 32
 epochs = 10
-tensorboard = TensorBoard(log_dir=f"logs/{time.time()}")
 
-# plt.bar(["Train", "Validation", "Test"], [sum([len(files) for r, d, files in os.walk(train_dir)]), sum([len(files) for r, d, files in os.walk(val_dir)]), sum([len(files) for r, d, files in os.walk(test_dir)])])
+def create_data(data_subset,img_size=128,rot_range=None,flip=True):
+    classes = ["WithMask", "WithoutMask"]
+    if data_subset == "train":
+        subset_path = ".\\dataset\\Train\\"
+    elif data_subset == "val":
+        subset_path = ".\\dataset\\Validation\\"
+    elif data_subset == "test":
+        subset_path = ".\\dataset\\Test\\"
+    else:
+        raise KeyError("Input is now one of the following categories: train, val, and/or test.")
 
-augment_train_data = ImageDataGenerator(rescale=1./255,
-                                        shear_range=0.15,
-                                        zoom_range=0.15,
-                                        horizontal_flip=True)
-augment_val_data = ImageDataGenerator(rescale=1./255,
-                                      shear_range=0.25,
-                                      zoom_range=0.25,
-                                      horizontal_flip=True)
-augment_test_data = ImageDataGenerator(rescale=1./255,)
+    X = []
+    y = []
+    for class_option in classes:
+        for filename in os.listdir(os.path.join(subset_path,class_option)):
+            counter = 0
+            img_path = os.path.join(os.path.join(subset_path,class_option),filename)
+            open_img = Image.open(img_path).resize((img_size,img_size))
+            X.append(np.asarray(open_img))
+            counter += 1
 
-train_set = augment_train_data.flow_from_directory(train_dir,
-                                                   target_size=(img_size,img_size),
-                                                   batch_size=batch_size,
-                                                   shuffle=False,
-                                                   class_mode='categorical')
-val_set = augment_val_data.flow_from_directory(val_dir,
-                                               target_size=(img_size,img_size),
-                                               batch_size=batch_size,
-                                               shuffle=False,
-                                               class_mode='categorical')
-test_set = augment_val_data.flow_from_directory(test_dir,
-                                                target_size=(img_size,img_size),
-                                                batch_size=batch_size,
-                                                shuffle=False,
-                                                class_mode='categorical')
+            if flip == True:
+                flip_img = ImageOps.flip(open_img)
+                mirror_img = ImageOps.mirror(open_img)
+                
+                X.append(np.asarray(flip_img))
+                X.append(np.asarray(mirror_img))
+                counter += 2
+            
+            if rot_range != None:
+                if rot_range%10 != 0:
+                    raise ValueError("Number must be divisable by 10. Please input a number that is divisable by 10.")
+                    for i in range(10,rot_range+10,10):
+                        rot_img = open_img.rotate(i)
+                        X.append(np.asarray(rot_img))
+                        counter += 1
+
+            for i in range(counter):
+                if classes.index(class_option) == 0:
+                    y.append([1,0])
+                else:
+                    y.append([0,1])
+
+    normalize_X = np.asarray(X) / 255.0
+    return normalize_X,np.asarray(y)
+
+
+X_train, y_train = create_data("train",img_size=img_size,rot_range=70,flip=True)
+X_val, y_val = create_data("val",img_size=img_size,rot_range=40,flip=True)
+X_test, y_test = create_data("test",img_size=img_size,rot_range=None,flip=False)
 
 model = Sequential()
-model.add(Conv2D(128,(3,3),input_shape=(img_size,img_size,3)))
+model.add(Conv2D(16,(3,3), padding='same',input_shape=(img_size,img_size,3)))
 model.add(Activation('relu'))
-model.add(Dropout(0.2))
 model.add(MaxPooling2D(2,2))
-
-model.add(Conv2D(64,(3,3)))
-model.add(Activation('relu'))
 model.add(Dropout(0.5))
-model.add(MaxPooling2D(2,2))
 
-model.add(Conv2D(64,(3,3)))
+model.add(Conv2D(64,(3,3), padding='same'))
 model.add(Activation('relu'))
-model.add(Dropout(0.5))
 model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
 
+model.add(Conv2D(64,(3,3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.5))
+model.add(BatchNormalization())
+
+model.add(Dropout(0.9))
 model.add(Flatten())
-model.add(Dense(256))
+model.add(Dense(512))
 model.add(Activation('relu'))
 model.add(Dense(2))
 model.add(Activation('softmax'))
 
-model.compile(loss="binary_crossentropy",
-              optimizer="adam",
+model.compile(loss="categorical_crossentropy",
+              optimizer=Adam(learning_rate=1e-5),
               metrics=["accuracy"])
 
 print("Model train: ")
-model.fit(train_set,
+model.fit(x=X_train,y=y_train,
+          validation_data=(X_val,y_val),
           epochs=epochs,
-          batch_size=batch_size,
-          validation_data=val_set,
-          callbacks=[tensorboard])
+          batch_size=batch_size)
 
 print("Model save: ")
 model.save("model.h5")
 print("Done")
 
 print("Model evaluation: ")
-model.evaluate(test_set)
+model.evaluate(x=X_test,y=y_test)
 
-predictions = model.predict(test_set)
+predictions = model.predict(X_test)
 y_pred = np.argmax(predictions, axis=1)
 print('Confusion Matrix: ')
-print(confusion_matrix(test_set.classes, y_pred))
+print(confusion_matrix(np.argmax(y_test,axis=1), y_pred))
 
 imported_model = tf.keras.models.load_model("model.h5")
 labels = {0:"Mask", 1:"No mask"}
@@ -103,7 +122,7 @@ labels = {0:"Mask", 1:"No mask"}
 fig = plt.figure(figsize=(20,20))
 for i in range(1,11):
     plt.subplot(5,2,i)
-    test_img = np.array(test_set[i+2][0][i+15])
+    test_img = Image.fromarray(np.uint8(X_test[i+585]*255.0))
     test_img = np.array(test_img).reshape(1,img_size,img_size,3)
     prediction = imported_model.predict(test_img)
     plt.imshow(np.squeeze(test_img),cmap="gray")
